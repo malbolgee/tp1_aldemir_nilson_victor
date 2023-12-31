4bf00fb3335e84d70f4de40a57d8a714e6360cb5
Malbolge <victor.gomes@icomp.ufam.edu.br>
2023-12-30 23:37:26 -0400

    feat: Implement insertions
    This commit implements the database insertions. Each table is populated
    with the data gathered from the amazon-meta.txt file. We try to make
    this process a little bit faster by using threads to parallelize the
    tabe insertion, unfortunately is not possible to do it for all table as
    some tables depend on data in another table (foreign key).
    
    Signed-off-by: Malbolge <victor.gomes@icomp.ufam.edu.br>

--- tp1_3.2.py
+++ tp1_3.2.py
@@ -1,25 +1,29 @@
-import psycopg2
-
 import re
+import time
+from datetime import timedelta
+from threading import Thread
+
+import psycopg2
+from psycopg2.extras import execute_values
 
 tables = '''
     CREATE TABLE IF NOT EXISTS Product(
-        asin TEXT PRIMARY KEY NOT NULL,
+        asin VARCHAR(10) PRIMARY KEY NOT NULL,
         title TEXT,
         product_group TEXT,
         salesrank INTEGER
     );
     CREATE TABLE IF NOT EXISTS Similar_(
-        product_asin TEXT NOT NULL,
-        similar_asin TEXT NOT NULL,
+        product_asin VARCHAR(10) NOT NULL,
+        similar_asin VARCHAR(10) NOT NULL,
         PRIMARY KEY(product_asin, similar_asin),
         FOREIGN KEY(product_asin) REFERENCES Product(asin)
     );
     CREATE TABLE IF NOT EXISTS Reviews(
         id SERIAL PRIMARY KEY NOT NULL,
-        product_asin TEXT NOT NULL, 
+        product_asin VARCHAR(10) NOT NULL, 
         date DATE NOT NULL,
-        id_client TEXT NOT NULL,
+        id_client VARCHAR(15) NOT NULL,
         rating INTEGER,
         votes INTEGER,
         helpful INTEGER,
@@ -30,7 +34,7 @@ tables = '''
         name TEXT
     );
     CREATE TABLE IF NOT EXISTS Products_per_category(
-        product_asin TEXT NOT NULL, 
+        product_asin VARCHAR(10) NOT NULL, 
         category_id INTEGER NOT NULL,
         PRIMARY KEY(product_asin, category_id),
         FOREIGN KEY(product_asin) REFERENCES Product(asin),
@@ -41,7 +45,6 @@ tables = '''
 LINE_HEADER_REGEX = re.compile(r'^(\w+):\s*(.+)$')
 CATEGORY_CONTENT_REGEX = re.compile(r'^(.*)\[(\d+?)]$')
 REVIEWS_CONTENT_REGEX = re.compile(r'^(\d{4})-(\d{1,2})-(\d{1,2}) \w+: (\w+) \w+: (\w+) \w+: (\w+) \w+: (\w+)$')
-
 
 products = []
 categories = set()
@@ -56,6 +59,8 @@ CUSTOMER_ID = 4
 RATING = 5
 VOTES = 6
 HELPFUL = 7
+
+projection = ["asin", "title", "group", "salesrank"]
 
 
 def normalize(line: str):
@@ -101,6 +106,53 @@ def read_data_from_file(file_name: str):
                     ))
 
 
+def insert_similar(cursor, connection):
+    print("Inserting into Similar...")
+    execute_values(cursor, "INSERT INTO Similar_ (product_asin,similar_asin) VALUES %s",
+                   list(similar))
+    connection.commit()
+    print("Done inserting into Similar")
+
+
+def insert_reviews(cursor, connection):
+    print("Inserting into Reviews...")
+    execute_values(cursor, "INSERT INTO Reviews (product_asin,date,id_client,rating,votes,helpful) VALUES %s",
+                   list(reviews))
+    connection.commit()
+    print("Done inserting into Reviews")
+
+
+def populate_database(cursor, connection):
+    print("Inserting into Product...")
+    execute_values(cursor, "INSERT INTO Product (asin,title,product_group,salesrank) VALUES %s",
+                   [tuple(product.get(arg) for arg in projection) for product in products]),
+    connection.commit()
+    print("Done inserting into Product")
+
+    p1 = Thread(target=insert_similar, args=(cursor, connection))
+    p2 = Thread(target=insert_reviews, args=(cursor, connection))
+
+    p1.start()
+    p2.start()
+
+    print("Inserting into Category...")
+    execute_values(cursor, "INSERT INTO Category (id,name) VALUES %s",
+                   list(categories))
+    connection.commit()
+    print("Done inserting into Category")
+
+    print("Inserting into Products per category...")
+    execute_values(cursor, "INSERT INTO Products_per_category (product_asin,category_id) VALUES %s",
+                   list(products_by_categories))
+    connection.commit()
+    print("Done inserting into Products per category")
+
+    p1.join()
+    p2.join()
+
+    print("All inserts finished!")
+
+
 def execute():
     cursor = None
     connection = None
@@ -122,6 +174,10 @@ def execute():
         print("Reading file...")
         read_data_from_file("./amazon-meta.txt")
         print("Done reading file")
+
+        start_time = time.monotonic()
+        populate_database(cursor, connection)
+        print(f"It took {timedelta(minutes=time.monotonic() - start_time)} minutes to populate the Database")
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgresSQL", error)
